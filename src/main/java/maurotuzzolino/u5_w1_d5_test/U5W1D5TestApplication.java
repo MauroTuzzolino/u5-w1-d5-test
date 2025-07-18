732033065dca4d149b5ed8d70074f75b58bfe83b
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-07-18 10:15:31 +0200

    Initial commit

---
+++ src/main/java/maurotuzzolino/u5_w1_d5_test/U5W1D5TestApplication.java
@@ -0,0 +1,13 @@
+package maurotuzzolino.u5_w1_d5_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1D5TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U5W1D5TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maurotuzzolino/u5_w1_d5_test/U5W1D5TestApplicationTests.java
@@ -0,0 +1,13 @@
+package maurotuzzolino.u5_w1_d5_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D5TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
